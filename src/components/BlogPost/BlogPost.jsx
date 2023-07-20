fc1e7f058935655c57494e2518d0674239349079
Rumon Mahmudul Hasan <rumonhasan@Rumons-MacBook-Pro.local>
2023-07-20 19:38:43 +0900

    added dark mode for better readablity of card components within each post

--- src/components/BlogPost/BlogPost.jsx
+++ src/components/BlogPost/BlogPost.jsx
@@ -1,22 +1,30 @@
-import { Typography, LinearProgress, Card, CardContent } from '@mui/material';
+import {
+  Typography,
+  LinearProgress,
+  Card,
+  CardContent,
+  Box,
+  Switch,
+} from '@mui/material';
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 import './BlogPostStyles.css';
 import { purifyDOMContent } from '../../utils/general';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const BlogPost = (props) => {
-  const {
-    categories,
-    content,
-    coverImage,
-    createdAt,
-    updatedAt,
-    id,
-    slug,
-    title,
-  } = props;
+  const { content, coverImage, createdAt, updatedAt, id, slug, title } = props;
   const [blogLoading, setBlogLoading] = useState(true);
   const [blogContent, setBlogContent] = useState(null);
+  const [postDarkMode, setPostDarkMode] = useState('light');
+  const customTheme = createTheme({
+    palette: {
+      mode: postDarkMode,
+    },
+  });
+  const handlePostThemeChange = () => {
+    setPostDarkMode(postDarkMode === 'light' ? 'dark' : 'light');
+  };
 
   // generating the blog content asynchronously in order to prevent initial render error
   useEffect(() => {
@@ -35,24 +43,43 @@ const BlogPost = (props) => {
   }, [content]);
 
   return (
-    <Card className="card-container">
-      <Typography variant="h3" sx={{ fontSize: { xs: '2rem', sm: '4rem' } }}>
-        {title}
-      </Typography>
-      <CardContent>
-        {' '}
-        {blogLoading ? (
-          <LinearProgress />
-        ) : (
-          <div
-            className="blog-content"
-            dangerouslySetInnerHTML={{
-              __html: purifyDOMContent(blogContent),
-            }}
-          />
-        )}
-      </CardContent>
-    </Card>
+    <ThemeProvider theme={customTheme}>
+      <Card className="card-container">
+        <Box
+          display={'flex'}
+          justifyContent={'space-between'}
+          alignItems={'center'}
+          width="100%"
+        >
+          <Typography
+            variant="h3"
+            sx={{ fontSize: { xs: '2rem', sm: '4rem' } }}
+          >
+            {title}
+          </Typography>
+          <Box>
+            <Switch
+              checked={postDarkMode === 'dark'}
+              onChange={handlePostThemeChange}
+            />
+          </Box>
+        </Box>
+
+        <CardContent>
+          {' '}
+          {blogLoading ? (
+            <LinearProgress />
+          ) : (
+            <div
+              className="blog-content"
+              dangerouslySetInnerHTML={{
+                __html: purifyDOMContent(blogContent),
+              }}
+            />
+          )}
+        </CardContent>
+      </Card>
+    </ThemeProvider>
   );
 };
 
